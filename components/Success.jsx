ef1adbf68c7ce90308e364faf8175d74216c6934
kantlori <atabeywade@gmail.com>
2024-12-27 14:06:40 +0300

    Success page done

---
+++ components/Success.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Success = () => {
+    return (
+        <>
+            <Alert>
+                This is a primary alert — check it out!
+            </Alert>
+            <h1>Login Successful!</h1>
+        </>
+    );
+};
+
+export default Success;
